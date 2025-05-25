d010f7791ff96e408f2e45f7956ead2d713c6909
SchiavonNathan <Nathanmestrepk@gmail.com>
2025-05-24 23:23:12 -0300

    refactor: likes refactor

--- src/likes/likes.service.ts
+++ src/likes/likes.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Like } from './likes.entity';
@@ -18,6 +18,52 @@ export class LikesService {
         private postagensService: PostagensService, // Injeção do PostagensService
     ) {}
 
-    
+    async getAllLikes(): Promise<Like[]> {
+        const likes = await this.likeRepository.find({
+            relations: ['postagem', 'usuario'],
+        });
+        return likes;
+    }
+
+    async getLikeByPostagemId(postagem_id: number): Promise<Like[]> {
+        if (isNaN(postagem_id)) {
+            throw new Error("O ID da postagem deve ser um número válido.");
+        }
+
+        const likes = await this.likeRepository.find({
+            where: { postagem: { id: postagem_id } },
+            relations: ["usuario", "postagem"],
+        });
+
+        return likes;
+    }
+
+    async toggleLike(postId: number, userId: number) {
+        const postagem = await this.postagemRepository.findOne({ where: { id: postId } });
+        const usuario = await this.userRepository.findOne({ where: { id: userId } });
+
+        if (!postagem || !usuario) {
+            throw new NotFoundException('Postagem ou usuário não encontrado');
+        }
+
+        const existingLike = await this.likeRepository.findOne({
+            where: { postagem: { id: postId }, usuario: { id: userId } },
+        });
+
+        if (existingLike) {
+            await this.likeRepository.delete(existingLike.id);
+        } else {
+            const like = this.likeRepository.create({ postagem, usuario });
+            await this.likeRepository.save(like);
+        }
+
+        const likesCount = await this.likeRepository.count({
+            where: { postagem: { id: postId } },
+        });
+
+        await this.postagemRepository.update(postId, { likesCount });
+
+        return likesCount
+    }
     
 }
